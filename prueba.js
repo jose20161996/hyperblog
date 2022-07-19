c8789d433a1032d21a5b6b5941d778a7dc50ec78
alpha2321 <nosoyyoperez@gmail.com>
2022-07-18 20:45:13 -0400

    se agregan 3 funciones :)

--- prueba.js
+++ prueba.js
@@ -1,3 +1,14 @@
 function sumar(a,b){
     return a+b
 }
+function dividir(d,a){
+    return a/d;
+}
+
+function eliminar_input(id){
+    document.getElementById(id).remove();
+}
+function agregar_string(cadena){
+    var x =""
+    return x.cadena;
+}
